7aed1c21953ef3aa9d0f20342ac775feb1f32250
phuong <phuonghd@dn.paracelsoft.com>
2021-10-14 21:02:25 +0700

    layout

---
+++ fe/src/components/admin/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div><Header /><Footer /><Sidebar /></div>
+</template>
+
+<script>
+import Header from "./Header";
+import Footer from "./Footer";
+import Sidebar from "./Sidebar";
+export default {
+  name: "Layout",
+  components: {
+    Header,
+    Footer,
+    Sidebar,
+  },
+};
+</script>
+
+<style lang="scss">
+// Import css slag
+@import "../../views/admin/admin.scss";
+</style>
---
+++ fe/src/components/auth/Layout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Layout Auth</div>
+</template>
+
+<script>
+export default {
+  name: "Layout Auth",
+};
+</script>
+
+<style>
+</style>
